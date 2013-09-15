263e0e3380ba34caab735207e18f82c274d9c1ec
Jason Benn <jasoncbenn@gmail.com>
2013-09-15 00:27:56 -0700

    Working, basic modal.

--- add-button.js
+++ add-button.js
@@ -2,9 +2,16 @@ $('.pagehead-actions').prepend('<li class="add-book-btn"><span class="minibutton
 
 var url = "/substack/node-browserify/blob/master/bin/advanced.txt"
 
+$('body').append(modal)
+
 // Given a url for a file in a repo, return the html for the lines in that file
-$('.pagehead-actions').on('click', '.add-book-btn', function(){
-  $.get(url, function(response) {
-    return $(response).find('pre').html()
-  })
+$('.pagehead-actions').on('click', '.add-book-btn', function() {
+  // $.get(url, function(response) {
+    // return $(response).find('pre').html()
+  // })
+	$('.md-modal').toggleClass('md-show');
+})
+
+$('body').on('click', '.md-close', function() {
+	$('.md-modal').toggleClass('md-show');
 })
