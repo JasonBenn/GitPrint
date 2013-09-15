fa36f094c15e17495ace5ab5894cfcc8a68e6960
Ryan Leahy <ryanleahy90@gmail.com>
2013-09-14 22:28:41 -0700

    included tokens in api call

--- api.js
+++ api.js
@@ -1,14 +1,14 @@
-var repo = 'pilgrim'
-var user = 'rleahy22'
+var repo = 'wtfsidtw'
+var user = 'claubacher'
 var clientID = '62b11d2267425d40826b'
 var clientSecret = 'a50963d2c2bdbc5783e4df1939be09c52808e0b3'
 
-$.get('https://api.github.com/repos/' + user + '/' + repo + '/commits', function(commits) {
+$.get('https://api.github.com/repos/' + user + '/' + repo + '/commits' + '?client_id=' + clientID + '&client_secret=' + clientSecret, function(commits) {
   var sha = (commits[0].sha);
-	$.get('https://api.github.com/repos/' + user + '/' + repo + '/git/trees/' + sha, function(data) {
+	$.get('https://api.github.com/repos/' + user + '/' + repo + '/git/trees/' + sha  + '?recursive=1&client_id=' + clientID + '&client_secret=' + clientSecret, function(data) {
 	  $.each(data.tree, function(index){
 	  	var fileUrl = data.tree[index].url;
-	  	$.get(fileUrl, function(gitFile) {
+	  	$.get(fileUrl + '?client_id=' + clientID + '&client_secret=' + clientSecret, function(gitFile) {
 		  console.log(gitFile);
 			});
 	  });
