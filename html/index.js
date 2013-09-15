cddd2be7cef0b33f7454a9f0a8de5971782739db
Chris <claubacher@gmail.com>
2013-09-15 09:45:48 -0700

    Fix sortable positioning

--- html/index.js
+++ html/index.js
@@ -2,7 +2,8 @@ var modal = '<div class="container md-modal"> \
 <div class="header-cat"><img src="https://googledrive.com/host/0B9bg70URlInBR00zUW9PYnBWLWM/octocat_300.png"><span class="logo">GitPrint</span></div>  \
 	<div class="md-content"> \
 	  <div class="sidebar"> \
-		  <ul id="sortable"><h2>Repos</h2> \
+      <h2 class="sidebar-heading">Repos</h2> \
+		  <ul id="sortable"> \
 				<li class="ui-state-default">node-browserify</li> \
 				<li class="ui-state-default">dnode</li> \
 				<li class="ui-state-default">node-optimist</li> \
