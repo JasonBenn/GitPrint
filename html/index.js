b395cad193bcc1fa8461733122f0316b2eead7fc
Jason Benn <jasoncbenn@gmail.com>
2013-09-15 09:26:00 -0700

    Boxes are checked by default.

--- html/index.js
+++ html/index.js
@@ -21,9 +21,9 @@ var modal = '<div class="container md-modal"> \
           </h3> \
           <div class="ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom"> \
             <ul> \
-              <li><input type="checkbox" id="root-1" name="root-1" value="readme"><label for="root-1">README</label><span class="description">Description</span></li> \
-              <li><input type="checkbox" id="root-2" name="root-2" value="index.js"><label for="root-2">index.js</label><span class="description">Description</span></li>\
-              <li><input type="checkbox" id="root-3" name="root-3" value="package.json"><label for="root-3">package.json</label><span class="description"Description</span></li> \
+              <li><input type="checkbox" checked id="root-1" name="root-1" value="readme"><label for="root-1">README</label><span class="description">Description</span></li> \
+              <li><input type="checkbox" checked id="root-2" name="root-2" value="index.js"><label for="root-2">index.js</label><span class="description">Description</span></li>\
+              <li><input type="checkbox" checked id="root-3" name="root-3" value="package.json"><label for="root-3">package.json</label><span class="description"Description</span></li> \
             </ul> \
           </div> \
           <h3 class="accordion-header ui-accordion-header ui-helper-reset ui-state-default ui-accordion-icons ui-corner-all"> \
@@ -32,9 +32,9 @@ var modal = '<div class="container md-modal"> \
           </h3> \
           <div class="ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom"> \
             <ul> \
-              <li><input type="checkbox" id="lib-1" name="el-1" value="stuff"><label for="lib-1">stuff</label></li> \
-              <li><input type="checkbox" id="lib-2" name="el-2" value="stuff.js"><label for="lib-2">stuff.js</label></li> \
-              <li><input type="checkbox" id="lib-3" name="root-3" value="stuff.json"><label for="lib-3">stuff.json</label></li> \
+              <li><input type="checkbox" checked id="lib-1" name="el-1" value="stuff"><label for="lib-1">stuff</label></li> \
+              <li><input type="checkbox" checked id="lib-2" name="el-2" value="stuff.js"><label for="lib-2">stuff.js</label></li> \
+              <li><input type="checkbox" checked id="lib-3" name="root-3" value="stuff.json"><label for="lib-3">stuff.json</label></li> \
             </ul> \
           </div> \
           <h3 class="accordion-header ui-accordion-header ui-helper-reset ui-state-default ui-accordion-icons ui-corner-all"> \
@@ -43,9 +43,9 @@ var modal = '<div class="container md-modal"> \
           </h3> \
           <div class="ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom"> \
             <ul> \
-              <li><input type="checkbox" id="bin-1" name="bin-1"><label for="lib-1">advanced.txt</label></li> \
-              <li><input type="checkbox" id="bin-2" name="bin-2"><label for="lib-2">args.js</label></li> \
-              <li><input type="checkbox" id="bin-3" name="bin-3"><label for="lib-3">cmd.js</label></li> \
+              <li><input type="checkbox" checked id="bin-1" name="bin-1"><label for="lib-1">advanced.txt</label></li> \
+              <li><input type="checkbox" checked id="bin-2" name="bin-2"><label for="lib-2">args.js</label></li> \
+              <li><input type="checkbox" checked id="bin-3" name="bin-3"><label for="lib-3">cmd.js</label></li> \
             </ul> \
           </div> \
         </div> \
