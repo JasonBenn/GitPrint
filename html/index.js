096b38673375e41b9a740f39086c169c1fd10b7c
Jason Benn <jasoncbenn@gmail.com>
2013-09-15 01:22:53 -0700

    Fix modal markup.

--- html/index.js
+++ html/index.js
@@ -1,57 +1,57 @@
-var modal =
-'<div class="container md-modal"> \
+var modal = '<div class="container md-modal"> \
 	<div class="md-content"> \
-  <div class="sidebar"> \
-	  <ul id="sortable"><h2>Repos</h2> \
-		<li class="ui-state-default">node-browserify</li> \
-		<li class="ui-state-default">dnode</li> \
-		<li class="ui-state-default">node-optimist</li> \
-		<li class="ui-state-default">GitPrint</li> \
-	  </ul> \
-  </div> \
-<div id="repo-contents"> \
-<p class="accordion-expand-holder"> \
-	<a class="accordion-expand-all" href="#">Expand all</a> \
-</p> \
-<div id="accordion" class="ui-accordion ui-widget ui-helper-reset"> \
- \
-	<h3 class="accordion-header ui-accordion-header ui-helper-reset ui-state-default ui-accordion-icons ui-corner-all"> \
-		<span class="ui-accordion-header-icon ui-icon ui-icon-triangle-1-e"></span> \
-		Section 1 \
-	</h3> \
-	<div class="ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom"> \
-		<p> \
-			Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer \
-			ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit \
-			amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut \
-			odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate. \
-		</p> \
+	  <div class="sidebar"> \
+		  <ul id="sortable"><h2>Repos</h2> \
+				<li class="ui-state-default">node-browserify</li> \
+				<li class="ui-state-default">dnode</li> \
+				<li class="ui-state-default">node-optimist</li> \
+				<li class="ui-state-default">GitPrint</li> \
+		  </ul> \
+	  </div> \
+		<div id="repo-contents"> \
+			<p class="accordion-expand-holder"> \
+				<a class="accordion-expand-all" href="#">Expand all</a> \
+			</p> \
+			<div id="accordion" class="ui-accordion ui-widget ui-helper-reset"> \
+				<h3 class="accordion-header ui-accordion-header ui-helper-reset ui-state-default ui-accordion-icons ui-corner-all"> \
+					<span class="ui-accordion-header-icon ui-icon ui-icon-triangle-1-e"></span> \
+					Section 1 \
+				</h3> \
+				<div class="ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom"> \
+					<p> \
+						Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer \
+						ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit \
+						amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut \
+						odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate. \
+					</p> \
+				</div> \
+				<h3 class="accordion-header ui-accordion-header ui-helper-reset ui-state-default ui-accordion-icons ui-corner-all"> \
+					<span class="ui-accordion-header-icon ui-icon ui-icon-triangle-1-e"></span> \
+					Section 2 \
+				</h3> \
+				<div class="ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom"> \
+					<p> \
+						Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer \
+						ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit \
+						amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut \
+						odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate. \
+					</p> \
+				</div> \
+				<h3 class="accordion-header ui-accordion-header ui-helper-reset ui-state-default ui-accordion-icons ui-corner-all"> \
+					<span class="ui-accordion-header-icon ui-icon ui-icon-triangle-1-e"></span> \
+					Section 3 \
+				</h3> \
+				<div class="ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom"> \
+					<p> \
+						Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer \
+						ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit \
+						amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut \
+						odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate. \
+					</p> \
+				</div> \
+			</div> \
+		</div> \
+	  <button class="md-close">Close</button> \
 	</div> \
-		<h3 class="accordion-header ui-accordion-header ui-helper-reset ui-state-default ui-accordion-icons ui-corner-all"> \
-		<span class="ui-accordion-header-icon ui-icon ui-icon-triangle-1-e"></span> \
-		Section 2 \
-	</h3> \
-	 <div class="ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom"> \
-		<p> \
-			Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer \
-			ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit \
-			amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut \
-			odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate. \
-		</p> \
-	</div> \
-		<h3 class="accordion-header ui-accordion-header ui-helper-reset ui-state-default ui-accordion-icons ui-corner-all"> \
-		<span class="ui-accordion-header-icon ui-icon ui-icon-triangle-1-e"></span> \
-		Section 3 \
-	</h3> \
-	 <div class="ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom"> \
-		<p> \
-			Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer \
-			ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit \
-			amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut \
-			odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate. \
-		</p> \
-	</div> \
-	</div> \
-  <button class="md-close">Close</button \
 </div> \
 <div class="md-overlay"></div>'
